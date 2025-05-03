e166638224fa05b0ecfbd8781125ed676b980fd4
mansi066 <mansisinghal623@gmail.com>
2025-05-03 12:22:23 +0530

    Update hero section:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,38 +1,56 @@
-import { curve } from '../assets';
+import { curve, heroBackground } from '../assets';
+import Button from './Button';
 import Section from './Section';
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-  <section
-  ClassName="pt-[12rem] -mt-[5.25]"
-  crosses
-  crossesOfset="lg:translate-y-[5.25rem]"
-  customPaddings
-  id="hero"
+    <Section
+      className="pt-[12rem] -mt-[5.25]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+          <h1 className="h1 mb-6">
+            Decoding the Future, One Byte at a Time
+            <br />
+            <span className="inline-block relative">
+              Rossum {" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Where curiosity meets code, and collaboration fuels growth.
+            Join Rossum, your gateway to endless possibilities.
+          </p>
+          <Button href="/" white>Join</Button>
+        </div>
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24"></div>
+        <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+          <div className="relative bg-n-8 rounded-[1rem]">
+            <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+            <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+              <img src="#" className="w-full" width={1024} height={490} alt="coding-hub" />
+            </div>
+          </div>
+        </div>
 
-  >
-   <div className="container relative">
-    <div className="relative z-1 max-w-[62rem] 
-    mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
-        <h1 className="h1 mb-6">Decoding the Future, One Byte at a Time
-            <br/>
-        <span className="inline-block relative">     Rossum {" "}
-            <img src={curve} 
-        className="absolute top-full left-0 w-full xl:-mt-2"
-        width={624}
-        height={28}
-        alt="Curve"
-        />
-        
-        </span>
-        </h1>
-        <p></p>
+        {/* Hero Background with Animation */}
+        <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+          <img src={heroBackground} className="w-full" width={1440} height={1800} alt="hero" />
+        </div>
+      </div>
+    </Section>
+  );
+};
 
-    </div>
-    </div> 
-</section>
-  )
-}
-
-export default Hero
+export default Hero;
