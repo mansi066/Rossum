a468d9298ef08e885ffdc6bcd47170a744d62c3a
mansi066 <mansisinghal623@gmail.com>
2025-05-03 06:55:46 +0530

    Add header

---
+++ src/assets/index.js
@@ -0,0 +1,86 @@
+import check from "./check.svg";
+
+import play from "./play.svg";
+import gradient from "./gradient.png";
+import grid from "./grid.png";
+import check2 from "./check-02.svg";
+import loading1 from "./loading-01.svg";
+import yourlogo from "./yourlogo.svg";
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import recording03 from "./recording-03.svg";
+import recording01 from "./recording-01.svg";
+import disc02 from "./disc-02.svg";
+import chromecast from "./chrome-cast.svg";
+import sliders04 from "./sliders-04.svg";
+import loading from "./loading.png";
+import background from "./background.jpg";
+
+import curve from "./hero/curve.png";
+import heroBackground from "./hero/hero-background.jpg";
+
+
+
+import notification1 from "./notification/image-1.png";
+import notification2 from "./notification/image-2.png";
+import notification3 from "./notification/image-3.png";
+import notification4 from "./notification/image-4.png";
+
+import benefitCard1 from "./benefits/card-1.svg";
+import benefitCard2 from "./benefits/card-2.svg";
+import benefitCard3 from "./benefits/card-3.svg";
+import benefitCard4 from "./benefits/card-4.svg";
+import benefitCard5 from "./benefits/card-5.svg";
+import benefitCard6 from "./benefits/card-6.svg";
+
+
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+
+export {
+  check,
+  check2,
+  loading1,
+ 
+  play,
+  gradient,
+  grid,
+  yourlogo,
+  homeSmile,
+  file02,
+  searchMd,
+  plusSquare,
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+  loading,
+  background,
+  curve,
+  
+  heroBackground,
+ 
+  
+  
+  notification1,
+  notification2,
+  notification3,
+  notification4,
+  benefitCard1,
+  benefitCard2,
+  benefitCard3,
+  benefitCard4,
+  benefitCard5,
+  benefitCard6,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,224 @@
+import {
+ 
+  chromecast,
+  disc02,
+  
+  discordBlack,
+  facebook,
+  file02,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  
+  plusSquare,
+  
+  recording01,
+  recording03,
+  
+  
+  searchMd,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Home",
+    url: "#home",
+  },
+  {
+    id: "1",
+    title: "About",
+    url: "#about",
+  },
+  {
+    id: "2",
+    title: "Events",
+    url: "#events",
+  },
+  {
+    id: "3",
+    title: "Our Team",
+    url: "#our-team",
+  },
+  {
+    id: "4",
+    title: "Contact Us",
+    url: "#contact-us",
+  },
+  {
+    id: "5",
+    title: "New account",
+    url: "#signup",
+    onlyMobile: true,
+  },
+  {
+    id: "6",
+    title: "Sign in",
+    url: "#login",
+    onlyMobile: true,
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Photo generating",
+  "Photo enhance",
+  "Seamless Integration",
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+
+
+export const collabText =
+  "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.";
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Seamless Integration",
+    text: collabText,
+  },
+  {
+    id: "1",
+    title: "Smart Automation",
+  },
+  {
+    id: "2",
+    title: "Top-notch Security",
+  },
+];
+
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Basic",
+    description: "AI chatbot, personalized recommendations",
+    price: "0",
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+  {
+    id: "1",
+    title: "Premium",
+    description: "Advanced AI chatbot, priority support, analytics dashboard",
+    price: "9.99",
+    features: [
+      "An advanced AI chatbot that can understand complex queries",
+      "An analytics dashboard to track your conversations",
+      "Priority support to solve issues quickly",
+    ],
+  },
+  {
+    id: "2",
+    title: "Enterprise",
+    description: "Custom AI chatbot, advanced analytics, dedicated account",
+    price: null,
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Ask anything",
+    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    
+  },
+  {
+    id: "1",
+    title: "Improve everyday",
+    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Connect everywhere",
+    text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    
+  },
+  {
+    id: "3",
+    title: "Fast responding",
+    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Ask anything",
+    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    backgroundUrl: "./src/assets/benefits/card-5.svg",
+    
+  },
+  {
+    id: "5",
+    title: "Improve everyday",
+    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    
+  },
+];
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: discordBlack,
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "#",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "#",
+  },
+];
