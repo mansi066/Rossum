048412e54c08e2e786c11615f0fea947764cfa7f
mansi066 <mansisinghal623@gmail.com>
2025-05-04 11:58:48 +0530

    Update events

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { curve } from '../assets';
 
-const Heading = ({ className, title }) => {
+const Heading = ({ className, title,subtitle }) => {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 flex flex-col items-center`}>
       {title && <h1 className="h1 text-center">{title}
@@ -12,7 +12,7 @@ const Heading = ({ className, title }) => {
                         alt="Curve"
                       /></h1>}
 
-     
+{subtitle && <h4 className="h4 text-center">{subtitle} </h4>}
     </div>
   );
 };
